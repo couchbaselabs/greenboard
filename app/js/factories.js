1bcfe816b47b5fe0f30187b9c5bec2ea047c34f9
Tommie McAfee <tommie@couchbase.com>
2015-11-13 09:22:08 -0500

    make factory.js a provider

--- app/js/factories.js
+++ app/js/factories.js
@@ -1,49 +1,44 @@
-var app = angular.module('greenboardFactories', ['greenboardServices']);
+angular.module('greenboardFactories', ['greenboardServices'])
 
+.provider('Data', [function (){
 
-app.factory('Data',  [function DataFactory() {
+    this.versions = [];
+    this.bucket = "server";
+    this.selectedVersion = null;
+    this.versionBuilds = [];
+    this.selectedBuildObj = null;
+    this.timelineAbsData = [];
+    this.timelineRelData = [];
+    this.refreshSidebar = false;
+    this.refreshTimeline = false;
+    this.refreshJobs = false;    
 
-    var api =
-      { init: function(){
-            this.versions = [];
-            this.bucket = "server";
-            this.selectedVersion = null;
-            this.versionBuilds = [];
-            this.selectedBuildObj = null;
-            this.timelineAbsData = [];
-            this.timelineRelData = [];
-            this.refreshSidebar = false;
-            this.refreshTimeline = false;
-            this.refreshJobs = false;
-        },
-        findBuildObj: function(build){
-
-            var _build = this.versionBuilds.filter(function(b){
-                if(b.Version == build){
-                    return true;
+    this.$get = function(){
+        return {
+            findBuildObj: function(build){
+                var _build = this.versionBuilds.filter(function(b){
+                    if(b.Version == build){
+                        return true;
+                    }
+                });
+                var rc;
+                if (_build.length == 0){
+                    rc = this.lastVersionBuild();
+                } else {
+                    rc = _build[0];
                 }
-            });
-            var rc;
-            if (_build.length == 0){
-                rc = this.lastVersionBuild();
-            } else {
-                rc = _build[0];
-            }
-            return rc;
-        },
-        bucket: "server",
-        lastVersionBuild: function(){
-            return lastEl(this.versionBuilds);
-        },
-        knownPlatforms: [],
-        knownCategories: []
-    };
+                return rc;
+            },
+            bucket: "server",
+            lastVersionBuild: function(){
+                return lastEl(this.versionBuilds);
+            },
+            knownPlatforms: [],
+            knownCategories: []
+        }
+    }
+}])
 
-    api.init();
-
-    return api;
-
-}]);
 
 function lastEl(a){
   if(a.length > 0) {
