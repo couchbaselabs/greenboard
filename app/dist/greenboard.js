e4a0f5ae193298e51eb8d2118754dab9995a0974
Tommie McAfee <tommie@couchbase.com>
2015-11-28 16:27:34 -0500

    toggle sidebar values by percentage

--- app/dist/greenboard.js
+++ app/dist/greenboard.js
@@ -390,6 +390,8 @@ angular.module('app.sidebar', [])
 	  		templateUrl: 'partials/sidebar.html',
 	  		link: function(scope, elem, attrs){
 
+	  		  scope.showPerc = false
+
 	  		  scope.$watch(function(){ return Data.getBuildBreakdown() }, 
             	function(breakdown){
             		// breakdown has changed
@@ -407,11 +409,77 @@ angular.module('app.sidebar', [])
   		restrict: 'E',
   		scope: {
   			item: "=",
-  			title: "="
+  			title: "=",
+  			asNum: "=showPerc"
   		},
-  		templateUrl: "partials/sidebar_item.html"
+  		templateUrl: "partials/sidebar_item.html",
+  		link: function(scope, elem, attrs){
+
+	  		scope.getNumOrPerc = function(key){
+	  			// toggle by number or percentage
+	  			var asNum = scope.asNum
+	  			var item = scope.item
+	  			if(!item){ return }
+	  			if(key=="pass"){
+	  				if(asNum){ return item.Passed }
+	  				return getPercOfValStr(item, item.Passed)
+	  			}
+	  			if(key=="fail"){
+	  				if(asNum){ return item.Failed }
+	  				return getPercOfValStr(item, item.Failed)
+	  			}
+	  			if(key=="pend"){
+	  				if(asNum){ return item.Pending }
+	  				return getPercOfValStr(item, item.Pending)
+	  			}
+	  		}
+  			scope.getRunPercent = function(){ return getItemPercStr(scope.item) }
+
+	  		// set item bg
+  			passPerc = scope.getNumOrPerc("pass")
+  			if(passPerc == 100){
+	  			scope.bgColor = "bg-success"
+	  		} else if(passPerc >= 70){
+	  			scope.bgColor = "bg-warning"
+	  		} else {
+	  			scope.bgColor = "bg-danger"
+	  		}
+
+  		}
   	}
   }])
+
+function getPercOfVal(item, val){
+  if (!item){
+    return 0;
+  }
+
+  var denom = item.Passed + item.Failed;
+  if(denom == 0){ return 0; }
+  return Math.floor(100*((val/denom).toFixed(2)));
+}
+
+function getPercOfValStr(item, val){
+  return getPercOfVal(item, val)+"%"
+}
+
+function getItemPerc(item){
+  if (!item){
+    return 0;
+  }
+
+  var total = item.Passed + item.Failed;
+  var denom = total + item.Pending;
+  if(denom == 0){ return 0; }
+
+  return Math.floor(100*((total/denom).toFixed(2)));
+}
+
+function getItemPercStr(item){
+	return getItemPerc(item)+"%"
+}
+
+
 angular.module('app.target', [])
 
   .directive('targetSelector', ['$state','ViewTargets', 'Data',
