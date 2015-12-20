5ed55227397e1774b4f2b1c480a01ed1b5015bf2
Tommie McAfee <tahmmee@gmail.com>
2015-12-19 22:32:24 -0500

    sort builds on xaxis

--- app/js/angular-plotly.js
+++ app/js/angular-plotly.js
@@ -19,6 +19,15 @@
                             var failed = FAIL_BAR_STYLE
                             var filterBy = Data.getBuildFilter()
                             var builds = Data.getVersionBuilds()
+                            builds.sort(function(a, b){
+                              if(a.build < b.build){
+                                return -1
+                              }
+                              if(a.build > b.build){
+                                return 1
+                              }
+                              return 0
+                            })
                             passed.x = failed.x = builds.map(function(b){ return b.build })
                             passed.y = builds.map(function(b){ return b.Passed })
                             failed.y = builds.map(function(b){ return b.Failed })
